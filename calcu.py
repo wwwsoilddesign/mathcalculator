cfe7d0531169ccbbbd5ffcb863e8987c2c212f71
kavinkangaraj <132247892+wwwsoilddesign@users.noreply.github.com>
2023-05-28 16:39:28 +0530

    Add files via upload
    Done by me! Is it a basic calculator it can do addition and subtraction and division,
     and multiplication and module, floor division, made with .python

---
+++ calcu.py
@@ -0,0 +1,40 @@
+print("Done By @wwwsoliddesing.")
+
+print("catch me on! ")
+
+print ("Instagram : pic_o_grapy,\n Twitter, : kavinGeeks \n Linkedin,: Kavin \n Github.  : wwwsoilddsign")
+
+print("_______________________________________ open calculator_____________________________________________________________")
+x1 = float(input("Enter First Number here: "))
+y2 = float(input("Enter Second number here: "))
+
+
+
+print(" Press 1 for Addition \n Press 2 for Subtrantion \n Press 3 for Multiplications \n Press 4 for Divsion \n press 5 for Modulus \n Press 6 floor Division")
+
+
+choice = int(input("enter your from 1-5: "))
+
+if choice  == 1:
+    print(x1 + y2)
+elif choice == 2:
+    print(x1 - y2)
+elif choice == 3:
+    print(x1 * y2)
+elif choice == 4:
+    print(x1 / y2)
+elif choice == 5:
+    print(x1 % y2)
+elif choice == 6:
+    print(x1 // y2 )
+   
+    
+else:
+    print("indalid Input")
+
+    
+
+
+    
+    
+    
